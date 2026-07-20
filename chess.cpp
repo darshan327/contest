e1162bdf2e585e2ab75110f2adfc419ab5b927b2
Darshan G.V <darshangv36@gmail.com>
2026-07-20 23:09:12 +0530

    chess

--- chess.cpp
+++ chess.cpp
@@ -1,4 +1,5 @@
-
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
@@ -6,3 +7,18 @@ public:
         return ((start[0] + start[1]) % 2) == ((target[0] + target[1]) % 2);
     }
 };
+
+  int main(){
+    Solution obj;
+    int n;
+    for(int i=0; i<2; i++){
+        vector<int> start(2), target(2);
+        cin >> start[i] >> target[i];
+        if(obj.canReach(start, target)){
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+  }
