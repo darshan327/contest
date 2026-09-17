fd8acb27cea2d0eff4cc8168b342652c669f750a
darshan <darshangv36@gmail.com>
2026-09-17 23:15:39 +0530

    cnt

--- 4048. Count Values With Equally Spaced Occurrences I.cpp
+++ 4048. Count Values With Equally Spaced Occurrences I.cpp
@@ -5,14 +5,14 @@ public:
         for(int i=0; i<nums.size(); i++){
             mp[nums[i]].push_back(i);
         }
-        int count = 0;
+        int cnt=0;
         for(auto p : mp){
           vector<int> res=p.second;
             if(res.size()==3){
                 if(res[1]-res[0]==res[2]-res[1]){
-                    count++;
+                    cnt++;
                 }
-            }
+                }
         }
         return count;
     }
